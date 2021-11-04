ede4aeb2f2aa24fac65cbd87b14e10fbff8a1fd1
rpatel1023 <patelrushi1023@gmail.com>
2021-11-04 11:55:56 -0500

    created dynamic modals and implemented create project feature

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -7,12 +7,14 @@ import {
   Container,
   Button,
   ButtonGroup,
-  Modal
+
 } from "react-bootstrap";
 import ProjectViewer from "../../components/ProjectViewer";
 import "./ProjectsPage.css";
 import Spacer from "react-spacer";
 import Cookies from "js-cookie";
+import { Modal,ModalManager,Effect} from 'react-dynamic-modal';
+
 
 const axios = require('axios');
 
@@ -31,23 +33,70 @@ async function getProjects(current_user) {
   return res;
 }
 
+async function createProject(name, desc, id, owner) {
+  console.log(name, desc, id, owner);
+  const params = JSON.stringify({
+    "project_name": name,
+    "project_description": desc,
+    "project_id": id,
+    "project_owner": owner
+  })
+  const res = await axios.post(`${url}/project/create`, params, {
+    "headers": {
+      "content-type": "application/json",
+    }
+  })
+  console.log(res);
+  window.location.reload();
+}
 
 
+function MyModal(props) {
+  return (
+    <Modal onRequestClose={props.onRequestClose} effect={Effect.ScaleUp}>
+      <h1>{props.project.project_name}</h1>
+      <br/>
+      <ProjectViewer/>
+      <br/>
+      <Button onClick={ModalManager.close}>Close</Button>
+    </Modal>
+  )
+}
 
 
 function Projects(props) {
-  const [show, setShow] = useState(false);
+  const [projectName, setProjectName] = useState();
+  const [projectDesc, setProjectDesc] = useState();
+  const [projectID, setProjectID] = useState();
   const [projects, setProjects] = useState([]);
+  const [hwsets, setHWSets] = useState([]);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  
 
   useEffect(async () => {
     let result = await getProjects(Cookies.get('user-token'));
     console.log(result["data"]);
     setProjects(result["data"]);
+    // will likely make a call for hardware sets here and populate state variable for access
   }, [])
 
+  // function renderModal(project, index) {   
+  //   return (
+  //     <Modal dialogClassName="modal-90w" size="lg" show={show} onHide={handleClose} key={index}>
+  //       <Modal.Header>
+  //         <Modal.Title>{project.project_name}</Modal.Title>
+  //       </Modal.Header>
+  //       <Modal.Body>
+  //         <ProjectViewer />
+  //       </Modal.Body>
+  //     </Modal>
+  //   );
+  // }
+
+  function openModal(project, index) {
+    console.log("hello")
+    ModalManager.open(<MyModal project={project} onRequestClose={() => true}/>)
+  }
 
   function renderProjects(project, index) {
     return (
@@ -57,7 +106,7 @@ function Projects(props) {
         <td>{project.project_id}</td>
         <td>
           <ButtonGroup aria-label="btnGroup">
-            <Button variant="outline-primary" onClick={handleShow}>Open</Button>
+            <Button variant="outline-primary" onClick={() => openModal(project, index)}>Open</Button>
             <Button variant="outline-danger">Delete</Button>
           </ButtonGroup>
         </td>
@@ -97,44 +146,24 @@ function Projects(props) {
             <div class="create-project">
               <h2>Create Project</h2>
               <Form>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlInput1"
-                >
+                <Form.Group className="mb-3">
                   <Form.Label>Project Name</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" onChange={(e) => setProjectName(e.target.value)}/>
                 </Form.Group>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlTextarea1"
-                >
+                <Form.Group className="mb-3">
                   <Form.Label>Project Description</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" onChange={(e) => setProjectDesc(e.target.value)}/>
                 </Form.Group>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlTextarea1"
-                >
+                <Form.Group className="mb-3">
                   <Form.Label>Project ID</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control type="text" onChange={(e) => setProjectID(e.target.value)}/>
                 </Form.Group>
-                <Button>Create</Button>
+                <Button onClick={() => createProject(projectName, projectDesc, projectID, Cookies.get('user-token'))}>Create</Button>
               </Form>
             </div>
           </Col>
-          <Modal
-            dialogClassName="modal-90w"
-            size="lg"
-            show={show}
-            onHide={handleClose}
-          >
-            <Modal.Header>
-              <Modal.Title>Project 1</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <ProjectViewer />
-            </Modal.Body>
-          </Modal>
+
+
         </Row>
       </div>
     </Container>
