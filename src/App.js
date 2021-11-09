02042e013f4a312b939afb5d283f08d3c7c39a4e
rpatel1023 <patelrushi1023@gmail.com>
2021-11-08 19:22:09 -0600

    Revert "Merge branch 'development' into projects-endpoint"
    This reverts commit 44adeeeca078c43546607b2010292ec163baa1bf, reversing
    changes made to 17e9f8d3f8d33b1cc9111e14ed9c37da2d344589.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import logo from "./logo.svg";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import Projects from "./pages/ProjectsPage/ProjectsPage";
-import Navigation from "./components/Navigation";
-import HWSets from "./pages/HWSetsPage/HWSetsPage";
-import "./App.css";
-import { useState } from "react";
-import Cookies from "js-cookie";
-import DBPage from "./pages/DatabasePage/DBPage";
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage'
+import Projects from './pages/ProjectsPage/ProjectsPage';
+import Navigation from './components/Navigation';
+import HWSets from './pages/HWSetsPage/HWSetsPage';
+import './App.css';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+
 
 function App() {
   const [token, setToken] = useState(false);
 
-  if (!Cookies.get("user-token")) {
-    return <LoginPage setToken={setToken} />;
+
+  if (!Cookies.get('user-token')) {
+    return(
+      <LoginPage setToken={setToken}/>
+    )
   }
 
   return (
     <div className="App">
-      <Navigation />
-
+      <Navigation/>
       <Router>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/hwsets" component={HWSets} />
-        <Route
-          exact
-          path="/projects"
-          render={() => <Projects username="Rushi" user={token} />}
-        />
-        <Route exact path="/sources" component={DBPage} />
+            <Route exact path='/hwsets' component={HWSets}/>
+            <Route exact path='/projects' render={() => <Projects username="Rushi" user={token}/>} /> 
       </Router>
     </div>
+    
   );
 }
 
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,26 +1,25 @@
-import React from "react";
-import { Row, Col, Form, Container, Modal, Button } from "react-bootstrap";
-import Cookies from "js-cookie";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { Row, Col, Form, Container, Modal, Button } from 'react-bootstrap';
+import Cookies from 'js-cookie';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import "./LoginPage.css";
-import axios from "axios";
-import Spacer from "react-spacer";
+import axios from 'axios';
 
-/**
+
+/** 
  *  This will authenticate the user, calling the authenticate() endpoint in Flask
  *  if the credentials are valid, we will return the user object
- *  if the credentials are invalid, we will return false.
+ *  if the credentials are invalid, we will return false. 
  */
 async function LoginUser(user, pass) {
-  const res = await axios
-    .get("https://barons461-backend.herokuapp.com/user/login", {
-      params: {
-        username: user,
-        password: pass
-      }
+    const res = await axios.get('https://barons461-backend.herokuapp.com/user/login', {
+        params: {
+            username: user,
+            password: pass
+        }   
     })
-
+    
     .then(function (response) {
         if (response["data"][0] === -1) {
             return null;
@@ -37,10 +36,14 @@ async function LoginUser(user, pass) {
 }
 
 export default function LoginPage(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    let history = useHistory();
 
     /**
      * On submission of the login form, we authenticate the user and determine if their credentials are correct
@@ -57,105 +60,71 @@ export default function LoginPage(props) {
             history.push("/projects");
             window.location.reload();
 
-  /**
-   * On submission of the login form, we authenticate the user and determine if their credentials are correct
-   * If the credentials are invalid the token will be set to false. Need to do some kind of error catching to tell the user
-   * that their credentials are invalid.
-   */
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const token = await LoginUser(username, password);
-    console.log("my token is " + token);
-    if (token) {
-      props.setToken(token);
-      console.log("Successfully logged in with token: " + token);
-      Cookies.set("user-token", token, { expires: 1 });
-      history.push("/projects");
-      window.location.reload();
-    } else {
-      props.setToken(false);
-      console.log("Authentication failed");
-      alert("Login failed. Re-enter your credentials or create an account.");
+        }
+        else {
+            props.setToken(false);
+            console.log("Authentication failed");
+            alert("Login failed. Re-enter your credentials or create an account.");
+        }
+        
+        
     }
-  }
 
-  return (
-    <Container>
-      <div>
-        <Row>
-          <Spacer height="18px" />
-        </Row>
-        <Row className="center">
-          <div>
-            <form className="form" onSubmit={handleSubmit}>
-              <Col>
-                <div className="login-form">
-                  <h1>Barons Team</h1>
-                  <Form>
-                    <Form.Group className="mb-3">
-                      <Form.Label>Username</Form.Label>
-                      <Form.Control
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        type="text"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </Form.Group>
-                    <Button type="button" onClick={handleSubmit}>
-                      Log in
-                    </Button>
-                    <p>Don't have an account?</p>
-                    <Button type="button" onClick={handleShow}>
-                      Register
-                    </Button>
-                  </Form>
-                </div>
-              </Col>
-            </form>
-          </div>
-        </Row>
-        <Row>
-          <form></form>
-        </Row>
-      </div>
-      <Modal
-        dialogClassName="modal-90w"
-        size="lg"
-        show={show}
-        onHide={handleClose}
-      >
-        <Modal.Header>
-          <Modal.Title>Create an account</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Button type="button">Register</Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    </Container>
-  );
+    return(  
+        <Container>
+            <div>
+                <Row className="center">
+                        <div>
+                            <form className="form" onSubmit={handleSubmit}>
+                                <Col>
+                                    <div className="login-form">
+                                        <h1>Barons Team</h1>
+                                        <Form>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Username</Form.Label>
+                                                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+                                            </Form.Group>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control type="text" value={password} onChange={e => setPassword(e.target.value)}/>
+                                            </Form.Group>
+                                            <Button type="button" onClick={handleSubmit}>Log in</Button>
+                                            <p>Don't have an account?</p>
+                                            <Button type="button" onClick={handleShow}>Register</Button>
+                                        </Form>
+                                    </div>
+                                </Col>
+                            </form>               
+                            
+                        </div>
+
+                </Row>
+                <Row>
+                    <form>
+
+                    </form>         
+                </Row>
+            </div>
+            <Modal dialogClassName="modal-90w" size="lg" show={show} onHide={handleClose}>
+                <Modal.Header>
+                <Modal.Title>Create an account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="text" value={password} onChange={e => setPassword(e.target.value)}/>
+                        </Form.Group>
+                        <Button type="button">Register</Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        </Container>            
+
+    );
 }
+
